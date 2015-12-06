68af9327a80ed73265f7a3002410bca438733068
bi9bamboo <bi9bamboo@gmail.com>
2015-12-07 00:49:18 +0900

    forth commit

---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>PHPサンプル</title>
+</head>
+<body>
+<p><?php echo 'add test' ?></p>
+<p><?php echo 'add test2' ?></p>
+<p><?php echo 'add test3' ?></p>
+<p><?php echo 'Hello, World!' ?></p>
+</body>
+</html>
